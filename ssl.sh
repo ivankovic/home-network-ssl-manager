8ed494f709d079aeba649685cefd5f1d77ff2f1e
Marko Ivankovic <marko@ivankovic.me>
2017-02-26 18:20:06 +0100

    Change the paths a bit, fix the messages.

--- ssl.sh
+++ ssl.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
 
-mkdir -p ./SECRET/pki/$1/ca/
-mkdir -p ./SECRET/pki/$1/issued/
+mkdir -p ./SECRET/ca/
+mkdir -p ./SECRET/devices/
 
-if [ ! -f ./SECRET/pki/$1/ca/ca.key ]; then
-  echo "Creating a new Certificate Authority for $1 devices..."
+if [ ! -f ./SECRET/ca/ca.key ]; then
+  echo "The CA Root certificate needs to be generated first."
 
-  echo "Creating $1 CA file/directory structure..."
-  mkdir -p ./SECRET/pki/$1/ca/archive
-  cp /dev/null ./SECRET/pki/$1/ca/ca.db
-  cp /dev/null ./SECRET/pki/$1/ca/ca.db.attr
-  echo "01" > ./SECRET/pki/$1/ca/crt.srl
-  echo "01" > ./SECRET/pki/$1/ca/crl.srl
+  echo "Creating CA file/directory structure..."
+  mkdir -p ./SECRET/ca/archive
+  cp /dev/null ./SECRET/ca/ca.db
+  cp /dev/null ./SECRET/ca/ca.db.attr
+  echo "01" > ./SECRET/ca/crt.srl
+  echo "01" > ./SECRET/ca/crl.srl
 
-  echo "Creating $1 CA Root Certificate Request and key..."
+  echo "Creating CA Root Certificate Request and key..."
 
   SAN="DNS:$DOMAIN" \
   openssl req -new \
-    -config ./SECRET/pki/$1/ca.conf \
-    -out ./SECRET/pki/$1/ca/ca.csr \
-    -keyout ./SECRET/pki/$1/ca/ca.key \
+    -config ./SECRET/ca.conf \
+    -out ./SECRET/ca/ca.csr \
+    -keyout ./SECRET/ca/ca.key \
     -reqexts ca_reqext \
     -subj "/O=Internal Network/CN=Internal Network/DC=$DOMAIN/" \
     -passout pass:$PKI_PASSWORD \
     -batch
 
-  echo "Creating $1 CA Root Certificate..."
+  echo "Creating CA Root Certificate..."
 
   SAN="DNS:$DOMAIN" \
   openssl ca -selfsign \
-    -config ./SECRET/pki/$1/ca.conf \
-    -in ./SECRET/pki/$1/ca/ca.csr \
-    -out ./SECRET/pki/$1/ca/ca.crt \
+    -config ./SECRET/ca.conf \
+    -in ./SECRET/ca/ca.csr \
+    -out ./SECRET/ca/ca.crt \
     -extensions ca_ext \
     -passin pass:$PKI_PASSWORD \
     -batch
 else
-  echo "Found $1 CA!"
+  echo "The CA Root Certificate is already present, no need to re-create."
 fi
 
-if [ ! -f ./SECRET/pki/$1/issued/$2.crt ]; then
+if [ ! -f ./SECRET/issued/$2.crt ]; then
   CN="$2.$DOMAIN"
   SUBJECT="/O=Internal Network/CN=$CN/DC=$DOMAIN/"
 
-  echo "Creating $1 DEVICE \"$2\" Certificate Request and key..."
+  echo "Creating DEVICE \"$2\" Certificate Request and key..."
 
   SAN="DNS:$CN" \
   openssl req -new \
-    -config ./SECRET/pki/$1/ca.conf \
-    -out ./SECRET/pki/$1/issued/$2.csr \
-    -keyout ./SECRET/pki/$1/issued/$2.key \
+    -config ./SECRET/ca.conf \
+    -out ./SECRET/issued/$2.csr \
+    -keyout ./SECRET/issued/$2.key \
     -reqexts device_reqext \
     -subj "$SUBJECT" \
     -nodes \
     -batch
 
-  echo "Creating $1 DEVICE \"$2\" Certificate..."
+  echo "Creating DEVICE \"$2\" Certificate..."
 
   SAN="DNS:$CN" \
   openssl ca \
-    -config ./SECRET/pki/$1/ca.conf \
-    -in ./SECRET/pki/$1/issued/$2.csr \
-    -out ./SECRET/pki/$1/issued/$2.crt \
+    -config ./SECRET/ca.conf \
+    -in ./SECRET/issued/$2.csr \
+    -out ./SECRET/issued/$2.crt \
     -extensions device_ext \
     -passin pass:$PKI_PASSWORD \
     -batch
