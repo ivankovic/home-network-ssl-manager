9e88c66b860df1bba4b036da1216cf68103bbae9
Marko Ivankovic <marko@ivankovic.me>
2017-12-13 23:23:02 +0100

    Add revocation to the shell script.

--- ssl.sh
+++ ssl.sh
@@ -24,85 +24,117 @@ check_config () {
 create_dirs () {
   mkdir -p ./SECRET/ca/
   mkdir -p ./SECRET/issued/
+  mkdir -p ./SECRET/ca/archive
+}
+
+ensure_clr_exists () {
+  if [ ! -f ./SECRET/ca/ca.crl ]; then
+    echo "CRL does not exist yet. Creating a new CRL."
+    openssl ca \
+      -config ./ca.conf \
+      -gencrl \
+      -out ./SECRET/ca/ca.crl
+  else
+    echo "CRL exists."
+  fi
 }
 
 issue () {
-if [ ! -f ./SECRET/ca/ca.key ]; then
-  echo "The CA Root certificate needs to be generated first."
+  if [ ! -f ./SECRET/ca/ca.key ]; then
+    echo "The CA Root certificate needs to be generated first."
 
-  echo "Creating CA file/directory structure..."
-  mkdir -p ./SECRET/ca/archive
-  cp /dev/null ./SECRET/ca/ca.db
-  cp /dev/null ./SECRET/ca/ca.db.attr
-  echo "01" > ./SECRET/ca/crt.srl
-  echo "01" > ./SECRET/ca/crl.srl
+    echo "Creating CA file/directory structure..."
+    cp /dev/null ./SECRET/ca/ca.db
+    cp /dev/null ./SECRET/ca/ca.db.attr
+    echo "01" > ./SECRET/ca/crt.srl
+    echo "01" > ./SECRET/ca/crl.srl
 
-  echo "Creating CA Root Certificate Request and key..."
+    echo "Creating CA Root Certificate Request and key..."
 
-  SAN="DNS:$DOMAIN" \
-  ON="$ORGANIZATION" \
-  openssl req -new \
-    -config ./ca.conf \
-    -out ./SECRET/ca/ca.csr \
-    -keyout ./SECRET/ca/ca.key \
-    -reqexts ca_reqext \
-    -subj "/O=$ORGANIZATION/CN=$ORGANIZATION/DC=$DOMAIN/" \
-    -passout pass:$PKI_PASSWORD \
-    -batch
+    SAN="DNS:$DOMAIN" \
+      ON="$ORGANIZATION" \
+      openssl req -new \
+      -config ./ca.conf \
+      -out ./SECRET/ca/ca.csr \
+      -keyout ./SECRET/ca/ca.key \
+      -reqexts ca_reqext \
+      -subj "/O=$ORGANIZATION/CN=$ORGANIZATION/DC=$DOMAIN/" \
+      -passout pass:$PKI_PASSWORD \
+      -batch
 
-  echo "Creating CA Root Certificate..."
+    echo "Creating CA Root Certificate..."
 
-  SAN="DNS:$DOMAIN" \
-  ON="$ORGANIZATION" \
-  openssl ca -selfsign \
-    -config ./ca.conf \
-    -in ./SECRET/ca/ca.csr \
-    -out ./SECRET/ca/ca.crt \
-    -extensions ca_ext \
-    -passin pass:$PKI_PASSWORD \
-    -batch
-else
-  echo "The CA Root Certificate is already present, no need to re-create."
-fi
+    SAN="DNS:$DOMAIN" \
+      ON="$ORGANIZATION" \
+      openssl ca -selfsign \
+      -config ./ca.conf \
+      -in ./SECRET/ca/ca.csr \
+      -out ./SECRET/ca/ca.crt \
+      -extensions ca_ext \
+      -passin pass:$PKI_PASSWORD \
+      -batch
 
-if [ ! -f ./SECRET/issued/$1.crt ]; then
-  CN="$1.$DOMAIN"
-  SUBJECT="/O=$ORGANIZATION/CN=$CN/DC=$DOMAIN/"
+    ensure_clr_exists
+  else
+    echo "The CA Root Certificate is already present, no need to re-create."
+  fi
 
-  echo "Creating DEVICE \"$1\" Certificate Request and key..."
+  if [ ! -f ./SECRET/issued/$1.crt ]; then
+    CN="$1.$DOMAIN"
+    SUBJECT="/O=$ORGANIZATION/CN=$CN/DC=$DOMAIN/"
 
-  SAN="DNS:$CN" \
-  ON="$ORGANIZATION" \
-  openssl req -new \
-    -config ./ca.conf \
-    -out ./SECRET/issued/$1.csr \
-    -keyout ./SECRET/issued/$1.key \
-    -reqexts device_reqext \
-    -subj "$SUBJECT" \
-    -nodes \
-    -batch
+    echo "Creating DEVICE \"$1\" Certificate Request and key..."
 
-  echo "Creating DEVICE \"$1\" Certificate..."
+    SAN="DNS:$CN" \
+      ON="$ORGANIZATION" \
+      openssl req -new \
+      -config ./ca.conf \
+      -out ./SECRET/issued/$1.csr \
+      -keyout ./SECRET/issued/$1.key \
+      -reqexts device_reqext \
+      -subj "$SUBJECT" \
+      -nodes \
+      -batch
 
-  SAN="DNS:$CN" \
-  ON="$ORGANIZATION" \
-  openssl ca \
-    -config ./ca.conf \
-    -in ./SECRET/issued/$1.csr \
-    -out ./SECRET/issued/$1.crt \
-    -extensions device_ext \
-    -passin pass:$PKI_PASSWORD \
-    -batch
+    echo "Creating DEVICE \"$1\" Certificate..."
 
-  echo "Packaging the certificate. You will need to choose a password here."
+    SAN="DNS:$CN" \
+      ON="$ORGANIZATION" \
+      openssl ca \
+      -config ./ca.conf \
+      -in ./SECRET/issued/$1.csr \
+      -out ./SECRET/issued/$1.crt \
+      -extensions device_ext \
+      -passin pass:$PKI_PASSWORD \
+      -batch
 
-  openssl pkcs12 -export -clcerts \
-    -in ./SECRET/issued/$1.crt \
-    -inkey ./SECRET/issued/$1.key \
-    -out ./SECRET/issued/$1.p12
-else
-  echo "SSL certificate for $1 already present."
-fi
+    echo "Packaging the certificate. You will need to choose a password here."
+
+    openssl pkcs12 -export -clcerts \
+      -in ./SECRET/issued/$1.crt \
+      -inkey ./SECRET/issued/$1.key \
+      -out ./SECRET/issued/$1.p12
+  else
+    echo "SSL certificate for $1 already present."
+  fi
+}
+
+revoke () {
+  if [ ! -f ./SECRET/ca/ca.key ]; then
+    echo "The CA Root certificate needs to be generated first."
+  else
+    ensure_clr_exists
+
+    echo "Revoking DEVICE \"$1\" Certificate..."
+
+    SAN="DNS:$CN" \
+      ON="$ORGANIZATION" \
+      openssl ca \
+      -config ./ca.conf \
+      -passin pass:$PKI_PASSWORD \
+      -revoke ./SECRET/issued/$1.crt \
+      -batch
+  fi
 }
 
 usage () {
@@ -111,7 +143,8 @@ usage () {
   echo ""
   echo "COMMAND is one of the following:"
   echo "  new [NAME] - Issue an SSL certificate for [NAME]."
-  echo "  remove [NAME] - Remove the SSL certificate for [NAME]."
+  echo "  remove [NAME] - Revoke the SSL certificate for [NAME]."
+  echo "  revoke [NAME] - Revoke the SSL certificate for [NAME]."
   echo "  purge - Delete EVERYTHING."
   echo "  list - List issued certificates."
   echo "  test - Test the config and system environment."
@@ -130,7 +163,11 @@ case $1 in
     echo "Done."
     ;;
   remove)
-    rm -v ./SECRET/issued/$2.*
+    revoke "$2"
+    echo "Done."
+    ;;
+  revoke)
+    revoke "$2"
     echo "Done."
     ;;
   purge)
